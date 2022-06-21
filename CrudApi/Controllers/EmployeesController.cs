6b589478a7e1c5cfc45e6ae3ca98c189bd92c786
HP <HP@DESKTOP-N7M5G1R>
2022-06-21 11:36:04 +0300

    Add project files.

---
+++ CrudApi/Controllers/EmployeesController.cs
@@ -0,0 +1,76 @@
+﻿using CrudApi.EmployeeData;
+using CrudApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrudApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+
+    {
+        private IEmployeeData _employeeData;
+        public EmployeesController(IEmployeeData employeeData)
+        {
+            _employeeData = employeeData;
+        }
+        [HttpGet]
+        [Route("api/[controller]")]
+        public IActionResult GetEmployees()
+
+        {
+            return Ok(_employeeData.GetEmployees());
+        }
+
+        [HttpGet]
+        [Route("api/[controller]/{id}")]
+        public IActionResult GetEmployee(Guid id)
+
+        {
+            var employee = _employeeData.GetEmployee(id);
+            if(employee!= null)
+            {
+                return Ok(_employeeData.GetEmployee(id));
+            }
+            return NotFound($"Employee with id:{id} was not found");
+        }
+
+        [HttpPost]
+        [Route("api/[controller]")]
+        public IActionResult GetEmployee(Employee employee)
+
+        {
+             _employeeData.AddEmployee(employee);
+            return Created(HttpContext.Request.Scheme +
+                "://" + HttpContext.Request.Host + HttpContext.Request.Path + "/" + employee.Id, employee);
+        }
+        [HttpDelete]
+        [Route("api/[controller]/{id}")]
+        public IActionResult DeleteEmployee(Guid id)
+
+        {
+            var employee = _employeeData.GetEmployee(id);
+            if(employee !=null)
+            {
+                _employeeData.DeleteEmployee(employee);
+                return Ok();
+            }
+            return NotFound($"Employee with id:{id} was not found");
+            }
+        [HttpPatch]
+        [Route("api/[controller]/{id}")]
+        public IActionResult EditEmployee(Guid id,Employee employee)
+
+        {
+            var existingEmployee = _employeeData.GetEmployee(id);
+            if (existingEmployee != null)
+            {
+                employee.Id = existingEmployee.Id;
+                _employeeData.EditEmployee(employee);
+            }
+            return Ok(employee);
+            }
+    }
+
+}
---
+++ CrudApi/EmployeeData/IEmployeeData.cs
@@ -0,0 +1,13 @@
+﻿using CrudApi.Models;  
+
+namespace CrudApi.EmployeeData
+{
+    public interface IEmployeeData
+    {
+        List<Employee> GetEmployees();
+        Employee GetEmployee(Guid id);
+        Employee AddEmployee(Employee employee);
+        void DeleteEmployee(Employee employee);
+        Employee EditEmployee(Employee employee);
+    }
+}
